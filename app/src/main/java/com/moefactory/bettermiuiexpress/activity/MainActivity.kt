0e6484cf597b0e8e7b7e6c490b28646b88c078b1
Robotxm <wanglinpei65@gmail.com>
2022-07-09 01:04:43 +0800

    feat: add status check, add YukiHookAPI link

--- app/src/main/java/com/moefactory/bettermiuiexpress/activity/MainActivity.kt
+++ app/src/main/java/com/moefactory/bettermiuiexpress/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import com.highcapable.yukihookapi.YukiHookAPI
+import com.moefactory.bettermiuiexpress.R
 import com.moefactory.bettermiuiexpress.base.ui.BaseActivity
 import com.moefactory.bettermiuiexpress.databinding.ActivityMainBinding
 
@@ -24,6 +25,26 @@ class MainActivity : BaseActivity<ActivityMainBinding>(false) {
         }
         viewBinding.btnBlog.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://moefactory.com")))
+        }
+        viewBinding.mcvYuki.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/fankes/YukiHookAPI")))
+        }
+
+        viewBinding.tvYukiVersion.text =
+            getString(R.string.yuki_version, YukiHookAPI.API_VERSION_NAME, YukiHookAPI.API_VERSION_CODE)
+
+        if (YukiHookAPI.Status.isModuleActive) {
+            viewBinding.tvStatus.setText(R.string.active)
+            viewBinding.tvStatusDescription.text = getString(
+                R.string.active_hook_framework_version,
+                YukiHookAPI.Status.executorName,
+                YukiHookAPI.Status.executorVersion
+            )
+            viewBinding.ivStatus.setImageResource(R.drawable.ic_active)
+        } else {
+            viewBinding.tvStatus.setText(R.string.inactive)
+            viewBinding.tvStatusDescription.setText(R.string.inactive_description)
+            viewBinding.ivStatus.setImageResource(R.drawable.ic_inactive)
         }
     }
 }
