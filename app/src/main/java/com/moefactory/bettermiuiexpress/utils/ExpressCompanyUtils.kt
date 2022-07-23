fd0af2bc6e4f9f72fe662e674bc94399fff927ae
Robotxm <wanglinpei65@gmail.com>
2022-07-23 21:35:36 +0800

    refactor: introduce ExpressCompany class

--- app/src/main/java/com/moefactory/bettermiuiexpress/utils/ExpressCompanyUtils.kt
+++ app/src/main/java/com/moefactory/bettermiuiexpress/utils/ExpressCompanyUtils.kt
@@ -1,43 +1,48 @@
 package com.moefactory.bettermiuiexpress.utils
 
-object ExpressCompanyUtils {
-    
-    private val convertMap: MutableMap<String, String> = mutableMapOf<String, String>().apply {
-        this[Kuaidi100Code.SHUN_FENG] = CaiNiaoCode.SHUN_FENG
-        this[Kuaidi100Code.SHEN_TONG] = CaiNiaoCode.SHEN_TONG
-        this[Kuaidi100Code.YUAN_TONG] = CaiNiaoCode.YUAN_TONG
-        this[Kuaidi100Code.ZHONG_TONG] = CaiNiaoCode.ZHONG_TONG
-        this[Kuaidi100Code.GUO_TONG] = CaiNiaoCode.GUO_TONG
-        this[Kuaidi100Code.DE_BANG] = CaiNiaoCode.DE_BANG
-        this[Kuaidi100Code.YOU_ZHENG_CN] = CaiNiaoCode.YOU_ZHENG_CN
-        this[Kuaidi100Code.YOU_ZHENG_IN] = CaiNiaoCode.YOU_ZHENG_IN
-        this[Kuaidi100Code.EMS] = CaiNiaoCode.EMS
-        this[Kuaidi100Code.EMS_IN] = CaiNiaoCode.EMS_IN
-        this[Kuaidi100Code.YUN_DA] = CaiNiaoCode.YUN_DA
-        this[Kuaidi100Code.HUI_TONG] = CaiNiaoCode.HUI_TONG
-        this[Kuaidi100Code.QUAN_FENG] = CaiNiaoCode.QUAN_FENG
-        this[Kuaidi100Code.RU_FENG_DA] = CaiNiaoCode.RU_FENG_DA
-        this[Kuaidi100Code.ZHONG_TIE] = CaiNiaoCode.ZHONG_TIE
-        this[Kuaidi100Code.UPS] = CaiNiaoCode.UPS
-        this[Kuaidi100Code.DHL] = CaiNiaoCode.DHL
-        this[Kuaidi100Code.TIAN_TIAN] = CaiNiaoCode.TIAN_TIAN
-        this[Kuaidi100Code.KUAI_JIE] = CaiNiaoCode.KUAI_JIE
-        this[Kuaidi100Code.JING_DONG] = CaiNiaoCode.JING_DONG
-        this[Kuaidi100Code.JIA_YI] = CaiNiaoCode.JIA_YI
-        this[Kuaidi100Code.BAI_SHI] = CaiNiaoCode.BAI_SHI
-        this[Kuaidi100Code.YOU_SU] = CaiNiaoCode.YOU_SU
-        this[Kuaidi100Code.WAN_XIANG] = CaiNiaoCode.WAN_XIANG
-        this[Kuaidi100Code.CHENG_BANG] = CaiNiaoCode.CHENG_BANG
-        this[Kuaidi100Code.SU_NING] = CaiNiaoCode.SU_NING
-        this[Kuaidi100Code.ZHI_MA_KAI_MEN] = CaiNiaoCode.ZHI_MA_KAI_MEN
-        this[Kuaidi100Code.AN_NENG] = CaiNiaoCode.AN_NENG
-        this[Kuaidi100Code.LONG_BANG] = CaiNiaoCode.LONG_BANG
-    }
+data class ExpressCompany(
+    val kuaiDi100Code: String?,
+    val cainiaoCode: String?
+)
 
-    object Kuaidi100Code {
+object ExpressCompanyUtils {
+
+    private val expressCompanies = listOf(
+        ExpressCompany(Kuaidi100Code.SHUN_FENG, CaiNiaoCode.SHUN_FENG),
+        ExpressCompany(Kuaidi100Code.SHEN_TONG, CaiNiaoCode.SHEN_TONG),
+        ExpressCompany(Kuaidi100Code.YUAN_TONG, CaiNiaoCode.YUAN_TONG),
+        ExpressCompany(Kuaidi100Code.ZHONG_TONG, CaiNiaoCode.ZHONG_TONG),
+        ExpressCompany(Kuaidi100Code.GUO_TONG, CaiNiaoCode.GUO_TONG),
+        ExpressCompany(Kuaidi100Code.DE_BANG, CaiNiaoCode.DE_BANG),
+        ExpressCompany(Kuaidi100Code.YOU_ZHENG_CN, CaiNiaoCode.YOU_ZHENG_CN),
+        ExpressCompany(Kuaidi100Code.YOU_ZHENG_IN, CaiNiaoCode.YOU_ZHENG_IN),
+        ExpressCompany(Kuaidi100Code.EMS, CaiNiaoCode.EMS),
+        ExpressCompany(Kuaidi100Code.EMS_IN, CaiNiaoCode.EMS_IN),
+        ExpressCompany(Kuaidi100Code.YUN_DA, CaiNiaoCode.YUN_DA),
+        ExpressCompany(Kuaidi100Code.HUI_TONG, CaiNiaoCode.HUI_TONG),
+        ExpressCompany(Kuaidi100Code.QUAN_FENG, CaiNiaoCode.QUAN_FENG),
+        ExpressCompany(Kuaidi100Code.RU_FENG_DA, CaiNiaoCode.RU_FENG_DA),
+        ExpressCompany(Kuaidi100Code.ZHONG_TIE, CaiNiaoCode.ZHONG_TIE),
+        ExpressCompany(Kuaidi100Code.UPS, CaiNiaoCode.UPS),
+        ExpressCompany(Kuaidi100Code.DHL, CaiNiaoCode.DHL),
+        ExpressCompany(Kuaidi100Code.TIAN_TIAN, CaiNiaoCode.TIAN_TIAN),
+        ExpressCompany(Kuaidi100Code.KUAI_JIE, CaiNiaoCode.KUAI_JIE),
+        ExpressCompany(Kuaidi100Code.JING_DONG, CaiNiaoCode.JING_DONG),
+        ExpressCompany(Kuaidi100Code.JIA_YI, CaiNiaoCode.JIA_YI),
+        ExpressCompany(Kuaidi100Code.BAI_SHI, CaiNiaoCode.BAI_SHI),
+        ExpressCompany(Kuaidi100Code.YOU_SU, CaiNiaoCode.YOU_SU),
+        ExpressCompany(Kuaidi100Code.WAN_XIANG, CaiNiaoCode.WAN_XIANG),
+        ExpressCompany(Kuaidi100Code.CHENG_BANG, CaiNiaoCode.CHENG_BANG),
+        ExpressCompany(Kuaidi100Code.SU_NING, CaiNiaoCode.SU_NING),
+        ExpressCompany(Kuaidi100Code.ZHI_MA_KAI_MEN, CaiNiaoCode.ZHI_MA_KAI_MEN),
+        ExpressCompany(Kuaidi100Code.AN_NENG, CaiNiaoCode.AN_NENG),
+        ExpressCompany(Kuaidi100Code.LONG_BANG, CaiNiaoCode.LONG_BANG),
+    )
+
+    private object Kuaidi100Code {
         const val AN_NENG = "annengwuliu"
         const val BAI_SHI = "baishiwuliu"
-        const val CHENG_BANG = "chengbang"
+        val CHENG_BANG = null // "chengbang" // Incorrect
         const val DE_BANG = "debangwuliu"
         const val DHL = "dhl"
         const val EMS = "ems"
@@ -47,26 +52,26 @@ object ExpressCompanyUtils {
         const val JIA_YI = "jiayiwuliu"
         const val JING_DONG = "jd"
         const val KUAI_JIE = "kuaijiesudi"
-        const val LONG_BANG = "longbangwuliu"
+        val LONG_BANG = null // "longbangwuliu" // Incorrect
         const val QUAN_FENG = "quanfengkuaidi"
-        const val RU_FENG_DA = "rufengda"
+        val RU_FENG_DA = null // "rufengda" // Incorrect
         const val SHEN_TONG = "shentong"
         const val SHUN_FENG = "shunfeng"
         const val SU_NING = "suning"
         const val TIAN_TIAN = "tiantian"
         const val UPS = "ups"
         const val WAN_XIANG = "wanxiangwuliu"
-        const val YOU_SU = "yousuwuliu"
+        val YOU_SU = null // "yousuwuliu" // Incorrect
         const val YOU_ZHENG_CN = "youzhengguonei"
         const val YOU_ZHENG_IN = "youzhengguoji"
         const val YUAN_TONG = "yuantong"
         const val YUN_DA = "yunda"
         const val ZHI_MA_KAI_MEN = "zhimakaimen"
-        const val ZHONG_TIE = "zhongtiekuaiyun"
+        val ZHONG_TIE = null // "zhongtiekuaiyun" // Incorrect
         const val ZHONG_TONG = "zhongtong"
     }
 
-    object CaiNiaoCode {
+    private object CaiNiaoCode {
         const val AN_NENG = "ANE56"
         const val BAI_SHI = "BESTQJT"
         const val CHENG_BANG = "CHENGBANG"
@@ -98,34 +103,7 @@ object ExpressCompanyUtils {
         const val ZHONG_TONG = "ZTO"
     }
 
-    fun convertState(i: Int): Int {
-        return when (i) {
-            0 -> 102
-            1 -> 104
-            2 -> 105
-            3 -> 109
-            4 -> 108
-            5 -> 107
-            6 -> 111
-            else -> -1
-        }
-    }
-
-    fun convertStateV2(i: Int): Int {
-        return when (i) {
-            0 -> 101
-            1 -> 103
-            2, 3, 4 -> 104
-            5 -> 105
-            6 -> 109
-            7 -> 108
-            8 -> 106
-            9 -> 107
-            else -> -1
-        }
-    }
-
     fun convertCode(cainiaoCode: String): String? {
-        return convertMap.filterValues { it == cainiaoCode }.keys.firstOrNull()
+        return expressCompanies.find { it.cainiaoCode == cainiaoCode }?.kuaiDi100Code
     }
 }
